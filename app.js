6114430a44e02ca0da52ba59bf0fd9a269137857
Laia Solanes <laiasolanes@gmail.com>
2022-10-10 15:24:06 +0200

    Prevent onresize amn device sizes to change height

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const boxes = document.querySelectorAll('.expand__box')
 
-console.table(boxes)
-
 function expandBox() {
-  event.preventDefault();
+  if (window.innerWidth <= 992) {
+    event.preventDefault();
 
-  if (this.classList.contains('expanded')) {
-    this.classList.remove('expanded')
-    this.setAttribute('style', 'height: 7rem')
-  } else {
-    const boxHeight = this.querySelector('.box__content').scrollHeight;
-    this.classList.add('expanded')
-    this.setAttribute('style', `height: ${boxHeight}px`)
-
+    if (this.classList.contains('expanded')) {
+      this.classList.remove('expanded')
+      this.removeAttribute('style', 'height')
+    } else {
+      const boxHeight = this.querySelector('.box__content').scrollHeight;
+      this.classList.add('expanded')
+      this.setAttribute('style', `height: ${boxHeight}px`)
+    }
   }
+}
 
-  // this.classList.toggle('expanded');
-  console.log(this.scrollHeight)
+function onResizeRemoveAttribute() {
+  boxes.forEach(box => {
+    box.classList.remove('expanded');
+    box.removeAttribute('style', 'height');
+  })
 }
 
 boxes.forEach(box => box.addEventListener('click', expandBox))
+window.addEventListener('resize', onResizeRemoveAttribute);
